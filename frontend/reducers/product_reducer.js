bf6608f3d543150a3081c4265880a74bc3d24c36
terrychow <terryhchow@gmail.com>
2020-11-24 16:01:06 -0800

    remove product reducer action

--- frontend/reducers/product_reducer.js
+++ frontend/reducers/product_reducer.js
@@ -1,22 +1,25 @@
-import { RECEIVE_ALL_PRODUCTS, RECEIVE_PRODUCT} from '../actions/product_index_actions';
-import { RECEIVE_CART_ITEMS } from '../actions/cart_actions';
+import {
+  RECEIVE_ALL_PRODUCTS,
+  RECEIVE_PRODUCT,
+} from "../actions/product_index_actions";
+import { RECEIVE_CART_ITEMS } from "../actions/cart_actions";
 
 const productReducer = (oldState = {}, action) => {
-    Object.freeze(oldState);
-    switch (action.type) {
-        case RECEIVE_ALL_PRODUCTS:
-            return action.products;
-        case RECEIVE_PRODUCT:
-            return action.product;
-        case RECEIVE_CART_ITEMS:
-            if (action.payload && action.payload.products) {
-                return action.payload.products;
-            } else {
-                return oldState
-            }
-        default:
-            return oldState;
-    }
-}
+  Object.freeze(oldState);
+  switch (action.type) {
+    case RECEIVE_ALL_PRODUCTS:
+      return action.products;
+    case RECEIVE_PRODUCT:
+      return action.product;
+    // case RECEIVE_CART_ITEMS:
+    //     if (action.payload && action.payload.products) {
+    //         return action.payload.products;
+    //     } else {
+    //         return oldState
+    //     }
+    default:
+      return oldState;
+  }
+};
 
 export default productReducer;
